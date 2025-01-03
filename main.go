8564d87a8d8c24a3c18a99dc4f7411a6fe43ca98
yaki <yasunofumi11@gmail.com>
2025-01-03 21:21:47 +0900

    DB接続

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gin-practice/controllers"
+	"gin-practice/infra"
 	"gin-practice/models"
 	"gin-practice/repositories"
 	"gin-practice/services"
@@ -10,6 +11,7 @@ import (
 )
 
 func main() {
+	infra.Initialize()
 	items := []models.Item{
 		{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
 		{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
